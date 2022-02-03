8f53cf280f7c74dc4983009979d5e1fff07821a4
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-03 14:46:50 -0800

    moved state management and useEffect to Regions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-import { fetchPokedexList, fetchRegionList } from './services/pokemon';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './views/Home/Home';
 import Generation from './views/Generation/Generation';
@@ -10,31 +8,13 @@ import PokeDetail from './components/PokeDetail/PokeDetail';
 import styles from './App.css';
 
 export default function App() {
-  const [pokedexes, setPokedexes] = useState([]);
-  const [regions, setRegions] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // Get Pokedexes
-  useEffect(() => {
-    const getPokedexes = async () => {
-      const pokedexList = await fetchPokedexList();
-      const regionList = await fetchRegionList();
-      setPokedexes(pokedexList);
-      setRegions(regionList);
-      setLoading(false);
-    };
-    getPokedexes();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-
   return (
     <Router>
       <div className={styles.App}>
         <div className={styles.left}>
           <h1>Pokémon Regions</h1>
           <Link to="/">Home</Link>
-          <Regions {...{ regions, pokedexes }} />
+          <Regions />
           <Route path="/:generation/:name" component={PokeDetail} />
         </div>
 
--- src/components/Regions/Regions.jsx
+++ src/components/Regions/Regions.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
+import { fetchPokedexList, fetchRegionList } from '../../services/pokemon';
 import { Link } from 'react-router-dom';
 import { sliceGeneration } from '../../utils/utils';
 
 import styles from './Regions.css';
 
-export default function Regions({ regions, pokedexes }) {
+export default function Regions() {
+  const [pokedexes, setPokedexes] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Get Pokedexes
+  useEffect(() => {
+    const getPokedexes = async () => {
+      const pokedexList = await fetchPokedexList();
+      const regionList = await fetchRegionList();
+      setPokedexes(pokedexList);
+      setRegions(regionList);
+      setLoading(false);
+    };
+    getPokedexes();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
   const filterRegions = regions.map((region) => {
     return pokedexes.find((dex) => {
       return dex.name.includes(region.name);
