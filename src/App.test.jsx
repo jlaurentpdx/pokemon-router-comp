1c4e2846ac7f3d75ecb94ffa67fff451dbaf85cd
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-03 18:44:40 -0800

    second behavioral test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import App from './App';
 // import { MemoryRouter, Route } from 'react-router-dom';
 
@@ -16,17 +16,25 @@ test('App renders a header to the page', async () => {
   expect(list.children).toHaveLength(8);
 });
 
-test.only('clicking on a region should render the correct list of Pokemon', async () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+test('clicking on a region should render the correct list of Pokemon', async () => {
+  render(<App />);
 
-  const kanto = await screen.findByRole('link', { name: /gen 1 \| kanto/i });
-
-  userEvent.click(kanto);
+  const region = await screen.findByRole('link', { name: /gen 1 \| kanto/i });
+  userEvent.click(region);
 
   const pokemon = await screen.findByRole('link', { name: /squirtle/i });
   expect(pokemon).toBeInTheDocument();
+});
+
+test.only('clicking on a region should render a pokemon detail view', async () => {
+  render(<App />);
+
+  const region = await screen.findByRole('link', { name: /gen 2 \| johto/i });
+  userEvent.click(region);
+
+  const pokemon = await screen.findByRole('link', { name: /totodile/i });
+  userEvent.click(pokemon);
+
+  const name = await screen.findByRole('heading', { name: /totodile/i });
+  expect(name).toBeInTheDocument();
 });
